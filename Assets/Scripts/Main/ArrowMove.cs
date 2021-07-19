df5e93905bef589a4c8b429ebe5458721f69fd81
박상빈 <jixg20@gmail.com>
2021-07-19 21:41:46 +0900

    no message

--- Assets/Scripts/Main/ArrowMove.cs
+++ Assets/Scripts/Main/ArrowMove.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class ArrowMove : MonoBehaviour
 {
-    private GameManager gameManager = null;
     private Vector2 diff = Vector2.zero;
     private float rotationZ = 180f;
     public bool stopRotation = false;
 
     void Start(){
-        gameManager = FindObjectOfType<GameManager>(); 
         gameObject.SetActive(false);
     }
     void Update(){
         if(stopRotation)return;
-        diff =gameManager.Player.transform.position -transform.position;
+        diff =GameManager.Instance.Player.transform.position -transform.position;
         rotationZ = Mathf.Atan2(diff.y,diff.x)*Mathf.Rad2Deg;
         gameObject.transform.rotation = Quaternion.Euler(0f,0f,rotationZ-90f);
     }
